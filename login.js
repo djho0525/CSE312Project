6ef921849a251415511488009c92bbf9975c7e7c
chris <cho29@buffalo.edu>
2021-10-15 15:14:32 -0400

    updated Form and added hide and show

---
+++ login.js
@@ -0,0 +1,9 @@
+function showLogin(){
+    elem=document.getElementById("loginForm")
+    elem.style.display = "block"
+}
+
+function showSignup(){
+    elem=document.getElementById("signUpForm")
+    elem.style.display = "block"
+}
