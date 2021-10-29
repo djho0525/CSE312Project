cc53478b2b2bd35d20a0bc3d42be08ef623072fc
Daniel Ho <djho@buffalo.edu>
2021-10-29 13:44:07 -0400

    Added HTTP Header Parser

--- util.py
+++ util.py
@@ -7,9 +7,6 @@ def writeBytes(filename, data):
     with open(filename, "wb") as file: file.write(data)
     file.close()
 
-
-
-
 def buffering(server, received_data):
     data = received_data
     while b'\r\n\r\n' not in data:
@@ -29,6 +26,41 @@ def parsing(data):
         name, value = f_input.split('=')
         form[name] = value
     return form
+
+# Parses HTTP Headers into a Dictionary
+def parseHeaders(headers):
+    headersDict = {}
+    for header in headers:
+        keyAndValue = header.split(":")
+        key = keyAndValue[0]
+        value = keyAndValue[1].strip()
+        # Extra check to check for "=" so that each ";" has a "=" to separate the name and value (used to exclude instances where the value may have "; " but isn't for additional options)
+        if value.find("; ") != -1 and (value.count("; ") == value.count("=")):
+            headersDict[key] = {}
+            headersDict[key]["value"] = value[:value.find("; ")].strip()
+            if value.count("; ") == 1:
+                headersDict[key]["extras"] = {}
+                extra = value[value.find("; ")+2:]
+                print(extra)
+                extraKeyAndValue = extra.split("=")
+                extraKey = extraKeyAndValue[0]
+                extraValue = extraKeyAndValue[1]
+                headersDict[key]["extras"][extraKey] = extraValue
+            else:
+                extraHeaders = value[value.find("; ")+2:].split("; ")
+                print(extraHeaders)
+                headersDict[key]["extras"] = {}
+                for extra in extraHeaders:
+                    extraKeyAndValue = extra.split("=")
+                    extraKey = extraKeyAndValue[0]
+                    extraValue = extraKeyAndValue[1]
+                    headersDict[key]["extras"][extraKey] = extraValue
+        else:
+            headersDict[key] = value
+    #print(headersDict)
+    return headersDict
+
+
 
 def querying(path):
     if len(path.split('?')) <= 1: return path, {}
