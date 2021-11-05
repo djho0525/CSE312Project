4f4e01baf0e27b73c0e64582e2406a8ca8af718b
megan9572 <megan9572@gmail.com>
2021-11-05 10:29:54 -0400

    added getResponse method and changed postResponse to 301 response

--- direct_messaging.py
+++ direct_messaging.py
@@ -2,11 +2,15 @@ import database as db
 import util
 import responses as r
 
-def postResponse(receiver, sender, message):
-    db.addMessage(receiver, sender, message)
-    messages = ''
-    for i in db.users[receiver].messages[sender]: messages += i + '<br/>'
-    content = util.readBytes("templates/messages.html")
-    content = content.decode().replace('{{message}}', messages).replace('{{receiver}}', receiver).replace("{{sender}}", sender).encode()
-    return r.response200("text/html", len(content), content)
+def getResponse(sender, receiver):
+    receiver = receiver.replace("%40", "@")
+    if receiver in r.activeUsers:
+        messages = ''
+        for message in db.getMessages(sender, receiver):
+            messages += "<b>" + message['user'] + "</b>" + message['content'] + '<br/>'
+        content = util.readBytes("templates/direct_messaging.html")
+        content = content.decode().replace('{{messages}}', messages).replace('{{receiver}}', receiver).encode()
+        return r.response200("text/html", len(content), content)
+    else:
+        return r.response404("User " + receiver + " does not exist or is inactive")
 
