78ddeefd6be1b6b25135d70fec98644fd9517931
megan9572 <megan9572@gmail.com>
2021-11-05 06:03:04 -0400

    changed responses of /login and /signup

--- login_signup.py
+++ login_signup.py
@@ -2,32 +2,32 @@ import database as db
 import responses as r
 
 def login(email, password):
+    email = email.replace("%40", "@")
     if db.userExists(email):
         user = db.getUser(email)
         if user.password == password:
-            email = email.replace("%40", "@")
             r.activeUsers.append('<a class ="dropdown-item" href="#" >' + email + '</a>')
             db.loginUser(email)
             r.currentUser.clear()
             r.currentUser.append(email)
-            return r.response301("/home".encode())
+            return r.response301("/home")
         else:
-            content = 'Login failed'
+            print('Login failed')
     else:
-        content = 'Email is not registered'
-    content = content.encode()
-    return r.response200("text/plain", len(content), content)
+        print('Email is not registered')
+    return r.response301("/")
 
 
 def signup(name, email, password, confirm_password):
-    if db.userExists(email): content = 'Email was already registered'
+    email = email.replace("%40", "@")
+    if db.userExists(email): print('Email was already registered')
     else:
         if password == confirm_password:
             db.addUser(email, password, name)
             db.loginUser(email)
             db.insertDefaultColor(email)
-            content = 'Created account successfully'
-        else: content = 'Passwords do not match'
-    content = content.encode()
-    return r. response200("text/plain", len(content), content)
+            print('Created account successfully')
+            return r.response301("/home")
+        else: print('Passwords do not match')
+    return r. response301("/")
 
