972f042dd571e266e3f217b83fcdf8e0f641bcc7
megan9572 <megan9572@gmail.com>
2021-12-10 18:02:30 -0500

    added password requirements

--- login_signup.py
+++ login_signup.py
@@ -2,6 +2,7 @@ import database as db
 import responses as r
 import secrets
 import bcrypt
+import re
 
 import util
 
@@ -29,7 +30,7 @@ def login(email, password):
 def signup(name, email, password, confirm_password):
     if db.userExists(email): print('Email was already registered')
     else:
-        if password == confirm_password:
+        if password == confirm_password and len(password) >= 8 and re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).+", password):
             # r.activeUsers[server] = email
             # db.addUserToRegister(email,name)
             userToken = secrets.token_urlsafe(32)
@@ -44,6 +45,6 @@ def signup(name, email, password, confirm_password):
             r.activeUsers.append(email)
 
             return r.response302("/home", "token="+str(userToken)+"; Max-Age=3600; HttpOnly")
-        else: print('Passwords do not match')
+        else: print('Passwords do not match or does not meet all requirements')
     return r. response302("/")
 
